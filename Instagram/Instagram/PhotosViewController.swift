5ee40cced857ffefd1f55b2d864c71a648fc2aa1
ngocdo <ngocdo@goappable.com>
2016-03-09 19:41:48 +0700

    First commit

---
+++ Instagram/Instagram/PhotosViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ngoc Do on 3/9/16.
+//  Copyright © 2016 com.appable. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
